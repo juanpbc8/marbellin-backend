354eeb9b021df1f6ab13cc7adb9578c160b5931d
juanp <jbaldeoncharri@gmail.com>
2025-07-04 15:35:32 -0500

    Añadido @CrossOrigin en ClienteController

--- src/main/java/com/marbellin/controller/ClienteController.java
+++ src/main/java/com/marbellin/controller/ClienteController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/clientes")
 @Tag(name = "Clientes", description = "Operaciones relacionadas con clientes")
